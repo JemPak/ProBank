17256bb08958f2ead6911634ff8f5bc5e305f68f
juanjomonsa45890 <juanjomonsa45890@gmail.com>
2021-10-27 01:41:39 -0500

    realizar transacciones a algun correo"

--- authApp/serializers/transactionSerializer.py
+++ authApp/serializers/transactionSerializer.py
@@ -1,16 +1,22 @@
 from authApp.models.account     import Account
+from authApp.models.user        import User
 from authApp.models.transaction import Transaction
 from rest_framework             import serializers
+from datetime                   import datetime
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Transaction
-        fields = ['origin_account', 'destiny_account', 'amount', 'register_date', 'note']
+        fields = ['origin_account','destiny_account', 'amount', 'register_date', 'note']
+
+    def transform_date(fecha):
+        return datetime.fromisoformat(fecha).strftime('%Y-%m-%d %H:%M:$S')
     
     def to_representation(self, obj):
         account_origin  = Account.objects.get(id=obj.origin_account_id)
         account_destiny = Account.objects.get(id=obj.destiny_account_id)
         transaction     = Transaction.objects.get(id=obj.id)
+        User_destiny    = User.objects.get(id=account_destiny.id)
         return {
             'id'            : transaction.id,
             'amount'        : transaction.amount,
@@ -26,6 +32,7 @@ class TransactionSerializer(serializers.ModelSerializer):
                 'id'            : account_destiny.id,
                 'balance'       : account_destiny.balance,
                 'lastChangeDate': account_destiny.lastChangeDate,
-                'isActive'      : account_destiny.isActive
+                'isActive'      : account_destiny.isActive,
+                'email'         : User_destiny.email 
             }
         }
--- authApp/views/transactionsView.py
+++ authApp/views/transactionsView.py
@@ -5,6 +5,7 @@ from rest_framework.permissions                       import IsAuthenticated
 from rest_framework_simplejwt.backends                import TokenBackend
 
 from authApp.models.account                           import Account
+from authApp.models.user                           import User
 from authApp.models.transaction                       import Transaction
 from authApp.serializers.transactionSerializer        import TransactionSerializer
 
@@ -46,7 +47,6 @@ class TransactionCreateView(generics.CreateAPIView):
     serializer_class   = TransactionSerializer
     permission_classes = (IsAuthenticated,)
 
-
     def post(self, request, *args, **kwargs):
         token        = request.META.get('HTTP_AUTHORIZATION')[7:]
         tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
@@ -60,7 +60,21 @@ class TransactionCreateView(generics.CreateAPIView):
         if origin_account.balance < request.data['transaction_data']['amount']:
             stringResponse = {'detail':'Saldo Insuficiente'}
             return Response(stringResponse, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+            #Try except por si la consulta arroja error al no encontrar nada
+        try:
+            User_email_destiny = request.data['transaction_data'].pop('email_destiny_account')
+            print(User_email_destiny)
+            User_destiny = User.objects.get(email=User_email_destiny)
+        except User_email_destiny.DoesNotExist:
+            User_destiny = None
         
+        if User_destiny == None:
+            stringResponse = {'detail':'Cuenta de envio inexistente'}
+            return Response(stringResponse, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        id_destiny_account = Account.objects.get(id=User_destiny.id)
+        request.data['transaction_data']['destiny_account'] = id_destiny_account.id
         serializer = TransactionSerializer(data=request.data['transaction_data'])
         serializer.is_valid(raise_exception=True)
         serializer.save()
